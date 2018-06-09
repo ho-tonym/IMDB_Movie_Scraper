5b80eacdc77e4aee78c0a5f0f74bcd2915b81226
ho-tonym <ho.tonym@gmail.com>
2018-06-09 00:26:47 -0400

    add nokogiri dependency

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,8 @@
 #Environment
 
-require_relative "../lib/version"
+require_relative '../lib/version'
 require_relative '../lib/cli'
+require_relative '../lib/movies'
+require 'pry'
+
+require 'nokogiri'
